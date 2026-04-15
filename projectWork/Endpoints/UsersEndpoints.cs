6681ce558e94f7a3005bcf8405b7531af544f163
alex <alex20log06.cir@gmail.com>
2026-04-15 17:24:03 +0200

    aggiunta email nella registrazione

--- projectWork/Endpoints/UsersEndpoints.cs
+++ projectWork/Endpoints/UsersEndpoints.cs
@@ -7,21 +7,21 @@ namespace projectWork.Endpoints;
 
 public static class UsersEndpoints
 {
-    record RegisterRequest(string Username, string Password);
+    record RegisterRequest(string Username, string Password, string Email);
     public static void AddUsersEndpoints(this IEndpointRouteBuilder route)
     {
         var group = route.MapGroup("/api/users");
 
         group.MapPost("/register", async Task<Results<Ok, BadRequest>> ([FromBody] RegisterRequest request, HttpContext context) =>
         {
-            if(request.Username.Trim() == "" || request.Password.Trim() == "")
+            if(request.Username.Trim() == "" || request.Password.Trim() == "" || request.Email.Trim() == "")
                 return TypedResults.BadRequest();
 
             var passwordServices = context.RequestServices.GetRequiredService<PasswordServices>();
             var (hash, salt) = passwordServices.CreateHash(request.Password.Trim());
 
             var usersServices = context.RequestServices.GetRequiredService<UsersServices>();
-            await usersServices.RegisterUser(request.Username.Trim(), hash, salt);
+            await usersServices.RegisterUser(request.Username.Trim(), hash, salt, request.Email.Trim());
 
             return TypedResults.Ok();
         });
--- projectWork/Services/UsersServices.cs
+++ projectWork/Services/UsersServices.cs
@@ -11,18 +11,18 @@ public class UsersServices
         _connectionString = configuration.GetConnectionString("db");
     }
 
-    public async Task RegisterUser(string username, string password_hash, string password_salt)
+    public async Task RegisterUser(string username, string password_hash, string password_salt, string email)
     {
         using var connection = new Npgsql.NpgsqlConnection(_connectionString);
         await connection.OpenAsync();
 
         string query = """
             INSERT INTO users
-                (username, password_hash, password_salt)
+                (username, password_hash, password_salt, email)
             VALUES
-                (@username, @password_hash, @password_salt);
+                (@username, @password_hash, @password_salt, @email);
             """;
 
-        await connection.ExecuteAsync(query, new {username, password_hash, password_salt});
+        await connection.ExecuteAsync(query, new {username, password_hash, password_salt, email});
     }
 }
