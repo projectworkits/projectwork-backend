edd6afff981ee0de0d06703eee9986e932c88042
alex <alex20log06.cir@gmail.com>
2026-04-16 14:50:39 +0200

    aggiunta titoli agli endpoint in swagger

--- projectWork/Authentication/AuthEndpoints.cs
+++ projectWork/Authentication/AuthEndpoints.cs
@@ -8,7 +8,7 @@ public static class AuthEndpoints
     record LoginRequest(string Username, string Password);
     public static void AddAuthenticationEndpoints(this IEndpointRouteBuilder route)
     {
-        var group = route.MapGroup("/api/auth");
+        var group = route.MapGroup("/api/auth").WithName("api authentication");
 
         group.MapGet("/refresh", async Task<Results<Ok, UnauthorizedHttpResult>> (HttpContext context) =>
         {
