15f95efb8fdc27e5064f9fe716ae1406be77057f
alex <alex.loggia.25@stud.itsaltoadriatico.it>
2026-04-17 22:59:54 +0200

    aggiornato model User

--- projectWork/Models/User.cs
+++ projectWork/Models/User.cs
@@ -5,7 +5,8 @@ public class User
     public int Id { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
-    public string Password { get; set; }
+    public string PasswordSalt { get; set; }
+    public string PasswordHash { get; set; }
     public DateTime Verified { get; set; }
     public bool Admin { get; set; }
     public bool Collaborator { get; set; }
