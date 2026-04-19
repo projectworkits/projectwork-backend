d06b81671af45c01ccd65d1907cee42168f804b4
alex <alex20log06.cir@gmail.com>
2026-04-19 21:48:08 +0200

    aggiunta endpoint prodotti

--- projectWork/Program.cs
+++ projectWork/Program.cs
@@ -58,6 +58,7 @@ app.UseAuthorization();
 app.AddAuthenticationEndpoints();
 app.AddUsersEndpoints();
 app.AddImagesEndpoints();
+app.AddProductsEndpoints();
 
 Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
